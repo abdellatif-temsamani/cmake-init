3d92494936ae12d3fcc2552b28ebc9972fe43a10
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2023-10-30 23:07:17 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// TODO: take args from command line (project name, etc.)
+// TODO: src/main.c (with main and hello world)
+// TODO: create CMakeLists.txt
+// TODO: create .gitignore
+// TODO: git init
+// NOTE: manage libs as well
+fn main() {
+    println!("Hello, world!");
+}
